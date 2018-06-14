1ac5b41dcaa5bd2814def8ba47ddb891fdc14c02
Matheus Sartori <ms.sartori@outlook.com.br>
2018-06-14 16:42:18 -0300

    Estados estáticos na listagem de autores

--- src/App.js
+++ src/App.js
@@ -4,69 +4,81 @@ import './css/side-menu.css';
 import './css/main.css';
 
 class App extends Component {
-  render() {
-    return (
-        <div id="layout">
-            <a href="#menu" id="menuLink" class="menu-link">
-                <span></span>
-            </a>
 
-            <div id="menu">
-                <div class="pure-menu">
-                    <a class="pure-menu-heading" href="#">Company</a>
+    constructor() {
+        super();
+        this.state = {lista : [{nome:'Matheus',email:'ms.sartori@outlook.com.br',senha:'123456'}]};
+    }
 
-                    <ul class="pure-menu-list">
-                        <li class="pure-menu-item"><a href="#" class="pure-menu-link">Home</a></li>
-                        <li class="pure-menu-item"><a href="#" class="pure-menu-link">Autores</a></li>
-                        <li class="pure-menu-item"><a href="#" class="pure-menu-link">Livros</a></li>
-                    </ul>
+    render() {
+        return (
+            <div id="layout">
+                <a href="#menu" id="menuLink" class="menu-link">
+                    <span></span>
+                </a>
+
+                <div id="menu">
+                    <div class="pure-menu">
+                        <a class="pure-menu-heading" href="#">Company</a>
+
+                        <ul class="pure-menu-list">
+                            <li class="pure-menu-item"><a href="#" class="pure-menu-link">Home</a></li>
+                            <li class="pure-menu-item"><a href="#" class="pure-menu-link">Autores</a></li>
+                            <li class="pure-menu-item"><a href="#" class="pure-menu-link">Livros</a></li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
 
-            <div id="main">
-                <div class="header">
-                <h1>Cadastro de Autores</h1>
-            </div>
-            <div class="content top-spacing" id="content">
-                <div class="pure-form pure-form-aligned">
-                    <form class="pure-form pure-form-aligned">
-                        <div class="pure-control-group">
-                            <label htmlFor="nome">Nome</label>
-                            <input id="nome" type="text" name="nome" value=""  />
-                        </div>
-                        <div class="pure-control-group">
-                            <label htmlFor="email">Email</label>
-                            <input id="email" type="email" name="email" value=""  />
-                        </div>
-                        <div class="pure-control-group">
-                            <label htmlFor="senha">Senha</label>
-                            <input id="senha" type="password" name="senha"  />
-                        </div>
-                        <div class="pure-control-group">
-                            <label></label>
-                            <button type="submit" class="pure-button pure-button-primary">Gravar</button>
-                        </div>
-                    </form>
+                <div id="main">
+                    <div class="header">
+                    <h1>Cadastro de Autores</h1>
                 </div>
-                <div>
-                    <table class="pure-table">
-                        <thead>
-                            <tr>
-                                <th>Nome</th>
-                                <th>email</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>Matheus</td>
-                                <td>ms.sartori@outlook.com.br</td>
-                            </tr>
-                        </tbody>
-                    </table>
+                <div class="content top-spacing" id="content">
+                    <div class="pure-form pure-form-aligned">
+                        <form class="pure-form pure-form-aligned">
+                            <div class="pure-control-group">
+                                <label htmlFor="nome">Nome</label>
+                                <input id="nome" type="text" name="nome" value=""  />
+                            </div>
+                            <div class="pure-control-group">
+                                <label htmlFor="email">Email</label>
+                                <input id="email" type="email" name="email" value=""  />
+                            </div>
+                            <div class="pure-control-group">
+                                <label htmlFor="senha">Senha</label>
+                                <input id="senha" type="password" name="senha"  />
+                            </div>
+                            <div class="pure-control-group">
+                                <label></label>
+                                <button type="submit" class="pure-button pure-button-primary">Gravar</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div>
+                        <table class="pure-table">
+                            <thead>
+                                <tr>
+                                    <th>Nome</th>
+                                    <th>email</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    this.state.lista.map(function(autor) {
+                                        return (
+                                            <tr>
+                                                <td>{autor.nome}</td>
+                                                <td>{autor.email}</td>
+                                            </tr>
+                                        );
+                                    })
+                                }
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     );
   }
 }
