641ca60812ddfce4f2d0dcd0a945c4e649683b22
Matheus Sartori <ms.sartori@outlook.com.br>
2018-06-14 21:06:02 -0300

    cors instalado na api para permitir todas origins nas requisições

--- api/config/custom-express.js
+++ api/config/custom-express.js
@@ -2,9 +2,12 @@ var express = require('express');
 var consign = require('consign');
 var bodyParser = require('body-parser');
 var validator = require('express-validator');
+var cors = require('cors');
 
 module.exports = function() {
 	var app = express();
+
+	app.use(cors());
 
 	app.use(bodyParser.urlencoded({extended: true}));
 	app.use(bodyParser.json());
