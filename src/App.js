922841df0fa6aa49c76c57f055a18fe0e15abc3a
Matheus Sartori <ms.sartori@outlook.com.br>
2018-06-21 02:01:42 -0300

    Refatoração (bug do map ainda presente)

--- src/App.js
+++ src/App.js
@@ -2,65 +2,10 @@ import React, { Component } from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
 import './css/main.css';
-import $ from 'jquery';
 
-import InputCustomForm from './components/InputCustomForm';
-import ButtonCustomForm from './components/ButtonCustomForm';
+import {FormularioAutores, TabelaAutores} from './Autor';
 
 class App extends Component {
-
-    constructor() {
-        super();
-        this.state = {lista : [], nome:'', email: '', senha: ''};
-        this.cadastraAutor = this.cadastraAutor.bind(this);
-        this.setNome = this.setNome.bind(this);
-        this.setEmail = this.setEmail.bind(this);
-        this.setSenha = this.setSenha.bind(this);
-    }
-
-    componentDidMount() {
-        $.ajax({
-            url: "http://localhost:8080/api/autores",
-            dataType: "json",
-            success: function(response) {
-                this.setState({lista:response});
-            }.bind(this)
-        });
-    }
-
-    cadastraAutor(event) {
-        event.preventDefault();
-
-        $.ajax({
-            url: "http://localhost:8080/api/autores",
-            contentType: "application/json",
-            dataType: "json",
-            type: "post",
-            data: JSON.stringify({
-                nome: this.state.nome,
-                email: this.state.email,
-                senha: this.state.senha
-            }),
-            success: function(response) {
-                this.setState({lista:response});
-            }.bind(this),
-            error: function(response) {
-                console.log('Erro');
-            }
-        });
-    }
-
-    setNome(event) {
-        this.setState({nome:event.target.value});
-    }
-
-    setEmail(event) {
-        this.setState({email:event.target.value});
-    }
-
-    setSenha(event) {
-        this.setState({senha:event.target.value});
-    }
 
     render() {
         return (
@@ -86,36 +31,8 @@ class App extends Component {
                     <h1>Cadastro de Autores</h1>
                 </div>
                 <div className="content top-spacing" id="content">
-                    <div className="pure-form pure-form-aligned">
-                        <form className="pure-form pure-form-aligned" onSubmit={this.cadastraAutor} method="post">
-                        <InputCustomForm id="nome" type="text" name="nome" label="Nome" value={this.state.nome} onChange={this.setNome}/>
-                        <InputCustomForm id="email" type="email" name="email" label="E-mail" value={this.state.email} onChange={this.setEmail}/>
-                        <InputCustomForm id="senha" type="password" name="senha" label="Senha" value={this.state.senha} onChange={this.setSenha}/>
-                        <ButtonCustomForm label="" buttonText="Gravar"/>
-                        </form>
-                    </div>
-                    <div>
-                        <table className="pure-table">
-                            <thead>
-                                <tr>
-                                    <th>Nome</th>
-                                    <th>E-mail</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    this.state.lista.map(function(autor) {
-                                        return (
-                                            <tr key={autor.id}>
-                                                <td>{autor.nome}</td>
-                                                <td>{autor.email}</td>
-                                            </tr>
-                                        );
-                                    })
-                                }
-                            </tbody>
-                        </table>
-                    </div>
+                <FormularioAutores/>
+                <TabelaAutores/>
                 </div>
             </div>
         </div>
