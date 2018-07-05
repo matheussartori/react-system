9660dc27da480e6fd6206a1bd12c320440b57c59
Matheus Sartori <ms.sartori@outlook.com.br>
2018-07-05 01:00:51 -0300

    Início implementação react-router

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import {Router,Route} from 'react-router';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
